fd397e68ff2f9e5c9fb2cb7df5797817f62511da
hussam Ibraheem <h.ibrahim14@outlook.com>
2024-08-08 14:08:50 +0300

    Add job Request

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,29 @@ import AddJobPage from "./pages/AddJobPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
 import NotfoundPage from "./pages/NotFoundPage";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/jobs" element={<JobPages />} />
-      <Route path="/add-job" element={<AddJobPage />} />
-      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
-      <Route path="*" element={<NotfoundPage />} />
-    </Route>
-  )
-);
 const App = () => {
+  const addJob = async (newJob) => {
+    const res = await fetch("/api/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobPages />} />
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route path="*" element={<NotfoundPage />} />
+      </Route>
+    )
+  );
+
   return <RouterProvider router={router} />;
 };
 
--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const AddJobPage = () => {
+const AddJobPage = ({ addJobSubmit }) => {
   const [title, setTitle] = useState("");
   const [type, setType] = useState("Full-Time");
   const [location, setLocation] = useState("");
@@ -11,30 +12,34 @@ const AddJobPage = () => {
   const [contactEmail, setContactEmail] = useState("");
   const [contactPhone, setContactPhone] = useState("");
 
-  const submitForm =(e)=>{
+const navigate = useNavigate();
+
+  const submitForm = (e) => {
     e.preventDefault();
 
-    const newJob ={
-        title,
-        type,
-        location,
-        description,
-        salary,
-        company:{
-            name: companyName,
-            description: companyDescription,
-            contactEmail,
-            contactPhone,
-        },
+    const newJob = {
+      title,
+      type,
+      location,
+      description,
+      salary,
+      company: {
+        name: companyName,
+        description: companyDescription,
+        contactEmail,
+        contactPhone,
+      },
     };
+    addJobSubmit(newJob);
+    return navigate('/jobs')
+  };
 
-  }
   return (
     <>
       <section className="bg-indigo-50">
         <div className="container m-auto max-w-2xl py-24">
           <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
-            <form onSubmit={submitForm} >
+            <form onSubmit={submitForm}>
               <h2 className="text-3xl text-center font-semibold mb-6">
                 Add Job
               </h2>
@@ -94,7 +99,9 @@ const AddJobPage = () => {
                   rows="4"
                   placeholder="Add any job duties, expectations, requirements, etc"
                   value={description}
-                  onChange={(e)=>{setDescription(e.target.value)}}
+                  onChange={(e) => {
+                    setDescription(e.target.value);
+                  }}
                 ></textarea>
               </div>
 
@@ -111,7 +118,7 @@ const AddJobPage = () => {
                   className="border rounded w-full py-2 px-3"
                   required
                   value={salary}
-                  onChange={(e)=>setSalary(e.target.value)}
+                  onChange={(e) => setSalary(e.target.value)}
                 >
                   <option value="Under $50K">Under $50K</option>
                   <option value="$50K - 60K">$50K - $60K</option>
@@ -139,7 +146,7 @@ const AddJobPage = () => {
                   placeholder="Company Location"
                   required
                   value={location}
-                  onChange={(e)=>setLocation(e.target.value)}
+                  onChange={(e) => setLocation(e.target.value)}
                 />
               </div>
 
@@ -159,7 +166,7 @@ const AddJobPage = () => {
                   className="border rounded w-full py-2 px-3"
                   placeholder="Company Name"
                   value={companyName}
-                  onChange={(e)=>setCompanyName(e.target.value)}
+                  onChange={(e) => setCompanyName(e.target.value)}
                 />
               </div>
 
@@ -177,7 +184,7 @@ const AddJobPage = () => {
                   rows="4"
                   placeholder="What does your company do?"
                   value={companyDescription}
-                  onChange={(e)=>setCompanyDescription(e.target.value)}
+                  onChange={(e) => setCompanyDescription(e.target.value)}
                 ></textarea>
               </div>
 
@@ -196,7 +203,7 @@ const AddJobPage = () => {
                   placeholder="Email address for applicants"
                   required
                   value={contactEmail}
-                  onChange={(e)=>setContactEmail(e.target.value)}
+                  onChange={(e) => setContactEmail(e.target.value)}
                 />
               </div>
               <div className="mb-4">
@@ -213,7 +220,7 @@ const AddJobPage = () => {
                   className="border rounded w-full py-2 px-3"
                   placeholder="Optional phone for applicants"
                   value={contactPhone}
-                  onChange={(e)=>setContactPhone(e.target.value)}
+                  onChange={(e) => setContactPhone(e.target.value)}
                 />
               </div>
 
